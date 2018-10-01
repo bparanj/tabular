a6f331b2f1edf9f1b103c08a285f2ead174d9d09
Bala Paranj <bparanj@Balas-MacBook-Pro.local>
2018-09-30 19:03:38 -0700

    Refactored solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,12 @@
 class UsersController < ApplicationController
   def index
-		tabular = Tabular.new(params)
+		data_table = UserDatatable.new(params)
 		
-		search_string = []
-		columns = %w(first_name last_name title city age start_date salary)
-		columns.each do |term|
-			search_string << "#{term} like :search"
-		end
-
-		table_headers = %w(first_name title city age start_date salary)
-		sort_column = table_headers[tabular.sort_column]
-		users = User.order("#{sort_column} #{tabular.sort_direction}")
-		users = users.limit(tabular.records_per_page).offset(tabular.offset)
-		users = users.where(search_string.join(' or '), search: "%#{tabular.search_term}%")
-		records_filtered = users.count
-		
-		data = []
-		users.each do |user|
-			data << ["#{user.first_name} #{user.last_name}", user.title, user.city, user.age, "#{user.start_date.strftime('%Y/%m/%d')}", user.salary]
-		end
-				
 		render json: {
-			"pageLength": tabular.records_per_page,
-			"recordsTotal": User.count,
-			"recordsFiltered": 100,
-			"data": data
+			"pageLength": data_table.page_length,
+			"recordsTotal": data_table.records_total,
+			"recordsFiltered": data_table.records_filtered,
+			"data": data_table.data
 		}
   end
 end
---
+++ app/lib/user_datatable.rb
@@ -0,0 +1,38 @@
+class UserDatatable
+  COLUMNS = %w(first_name last_name title city age start_date salary)
+  TABLE_HEADERS = %w(first_name title city age start_date salary)
+  
+  def initialize(hash)
+    @tabular = Tabular.new(hash)
+    @search_string = []
+    COLUMNS.each do |term|
+    	@search_string << "#{term} like :search"
+    end
+  end  
+  
+  def data
+    sort_column = TABLE_HEADERS[@tabular.sort_column]
+
+    users = User.order("#{sort_column} #{@tabular.sort_direction}")
+    users = users.limit(page_length).offset(@tabular.offset)
+    users = users.where(@search_string.join(' or '), search: "%#{@tabular.search_term}%")
+
+    result = []
+    users.each do |user|
+    	result << ["#{user.first_name} #{user.last_name}", user.title, user.city, user.age, "#{user.start_date.strftime('%Y/%m/%d')}", user.salary]
+    end    
+    result
+  end
+  
+  def page_length
+    @tabular.records_per_page
+  end
+  
+  def records_filtered
+    100
+  end
+  
+  def records_total
+    User.count
+  end
+end
